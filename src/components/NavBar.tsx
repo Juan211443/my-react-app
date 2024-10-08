84384a53287d6343d8bdc6fe24c0a07e9308596f
Juan Camilo Amaya Palacios <amayajuan984@gmail.com>
2024-10-07 19:28:35 -0500

    Layout UI

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,14 @@
-import { HStack, Image } from "@chakra-ui/react"
+import { VStack, Image } from "@chakra-ui/react"
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 
 const NavBar = () => {
     return (
-        <HStack justifyContent={"space-between"} padding='10px'>
+        <VStack justifyContent={"space-between"} padding='10px'>
             <Image src={logo} boxSize='60px'></Image>
             <ColorModeSwitch />
-        </HStack>
+        </VStack>
     )
 }
 
